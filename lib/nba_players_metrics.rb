e7cbe2c5ea11d3b468933ec0c721ee0cf130b4b7
muzeel <vasilets.4@mail.ru>
2025-05-27 21:27:24 +0300

    Initial commit

---
+++ lib/nba_players_metrics.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "nba_players_metrics/version"
+
+module NbaPlayersMetrics
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/nba_players_metrics/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module NbaPlayersMetrics
+  VERSION = "0.1.0"
+end
